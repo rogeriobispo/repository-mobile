9fb9e746ec96bd4893b7bd9b66ec102c14cd04ca
Rogerio Bispo <rogerio_pd@yahoo.com.br>
2020-04-27 00:49:00 -0300

    app done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Api from './services/api'
 
 import {
   SafeAreaView,
@@ -11,45 +12,77 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+
+    const loadProjects = () => {
+      Api.get('/repositories').then(response => {
+        setRepositories(response.data)
+      })
+    }
+
+    loadProjects()
+  }, [])
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const repos = repositories
+    for (var index in repos) {
+      if (repos[index].id === id) {
+        repos[index].likes = repos[index].likes + 1
+        break
+      }
+    }
+    console.log(repos)
+    setRepositories([...repos])
+    await Api.post(`/repositories/${id}/like`)
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        <FlatList
+          data={repositories}
+          keyExtractor={repository => repository.id}
+          renderItem={({ item: repository }) => (
+            <View style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{repository.title}</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+              <View style={styles.techsContainer}>
+                <FlatList
+                  data={repository.techs}
+                  keyExtractor={techs => techs}
+                  renderItem={({ item: techs }) => (
+                    <Text style={styles.tech}>
+                      {techs}
+                    </Text>
+                  )}
+                />
+              </View>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                  testID={`repository-likes-${repository.id}`}
+                >
+                  {repository.likes} {Number(repository.likes) > 0 ? 'curtidas' : 'curtida'}
+                </Text>
+              </View>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(repository.id)}
+                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                testID={`like-button-${repository.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+
+        />
       </SafeAreaView>
     </>
   );
@@ -82,6 +115,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 5,
     color: "#fff",
+    marginTop: 10,
   },
   likesContainer: {
     marginTop: 15,
---
+++ src/readme.md
@@ -0,0 +1,11 @@
+## desafio :rocket: RocketSeat :rocket:
+
+
+react native app to list repositories and add likes.
+
+
+## Tecnologias.
+- React Native
+- eslint
+- prettier
+
